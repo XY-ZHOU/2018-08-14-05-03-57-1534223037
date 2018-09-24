7c2b84126ad9a333a12ce6f5a51d9d3e7f0fed57
XY-ZHOU <731986251@qq.com>
2018-09-24 21:07:57 +0800

    qiuhe

--- main/main.js
+++ main/main.js
@@ -1,3 +1,9 @@
-module.exports = function main() {
+module.exports = function main(str) {
   // Write your code here
+    var arr=str.split('');
+    var sum=0;
+    for(var i=0;i<arr.length;i++){
+        sum+=arr[i];
+    }
+    return sum;
 };
